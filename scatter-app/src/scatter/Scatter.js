b4f86f30bbd198d6c3898e60828bef3c2cbbfbaf
yo16 <xxyo1xx@gmail.com>
2024-01-15 17:20:09 +0900

    draw graph with react-plotly

--- scatter-app/src/scatter/Scatter.js
+++ scatter-app/src/scatter/Scatter.js
@@ -1,9 +1,25 @@
-import Plotly from 'plotly.js-dist';
+import Plot from 'react-plotly.js';
 
 const Scatter = () => {
+    const data = [
+        {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: 'red'},
+        },
+        {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+    ];
+    const layout = {width: 320, height: 240, title: 'A Fancy Plot'};
+
     return (
         <>
-        Hello, I'm Scatter!
+        Hello, I'm Scatter!<br />
+        <Plot
+            data={data}
+            layout={ layout }
+        />
         </>
     );
 };
