383c8f05565f82b23c85dc74ed2c78a014192a25
yo16 <xxyo1xx@gmail.com>
2024-01-22 12:16:53 +0900

    公開ファイルをフォルダに移動

--- server-app/routes/index.js
+++ server-app/routes/index.js
@@ -1,10 +1,13 @@
+var path = require("path");
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  res.sendFile("index.html");
+  res.sendFile("index.html", {
+    root: path.join("public","scatter-app-build"),
+  });
 });
+router.use(express.static(path.join("public","scatter-app-build")));
 
 module.exports = router;
